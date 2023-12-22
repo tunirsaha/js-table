4b1e2ca4c0844f85b50421aa0c0c0f9a50e9f0e1
Tunir Saha <tunir.saha@ibm.com>
2023-12-23 02:01:13 +0530

    changes

--- script.js
+++ script.js
@@ -1,94 +1,114 @@
-
-// The lines enclosed needs to be initialized
-const apiUrl = 'https://jsonplaceholder.typicode.com/users';
-const config = [
+const THEAD = document.getElementById('thead')
+const TBODY = document.getElementById('tbody')
+const SAMPLE_API_URL = 'https://jsonplaceholder.typicode.com/users'
+const SAMPLE_TABLE_CONFIG = `[
     {
-        dataKey: 'id',
-        columnName: 'ID'
+        "dataKey": "id",
+        "columnName": "ID"
     },
     {
-        dataKey: 'name',
-        columnName: 'Name',
-        isSortable: true
+        "dataKey": "name",
+        "columnName": "Name",
+        "isSortable": true
     },
     {
-        dataKey: 'phone',
-        columnName: 'Phone'
+        "dataKey": "phone",
+        "columnName": "Phone"
     },
     {
-        dataKey: 'email',
-        columnName: 'Email'
+        "dataKey": "email",
+        "columnName": "Email"
     }
-];
-// The lines enclosed needs to be initialized 
+]`
 
-const thead = document.getElementById('thead')
-const tbody = document.getElementById('tbody')
-let sortOrder = 1 //-1 for desc, 1 for asc
-let tableData = [];
-let sortingColumn = {};
+let SORT_ORDER = 1 //-1 for desc, 1 for asc
+let TABLE_DATA = []
+let SORT_COLUMN = ''
+let API_URL = ''
+let TABLE_CONFIG = {}
+
+initSettings();
+
+function initSettings() {
+    document.getElementById('apiInput').value = SAMPLE_API_URL
+    document.getElementById('configInput').innerHTML = SAMPLE_TABLE_CONFIG
+}
+
+function getUserInput() {
+    cleanSlate()
+    API_URL = document.getElementById('apiInput').value
+    TABLE_CONFIG = JSON.parse(document.getElementById('configInput').value)
+    loadData()
+}
+
+async function loadData() {
+    TABLE_DATA = await fetchApiData(API_URL)
+    renderTable(TABLE_DATA)
+}
 
 async function fetchApiData(url) {
     return await fetch(url).then((res) => { return res.json() }).then((data) => { return data }).catch((err) => { console.log(err) })
 }
 
-function renderTable(theadNode, tbodyNode, configObj, data) {
+function renderTable(data) {
     if (data.length > 0) {
-        createTableHead(theadNode, configObj)
-        createTableBody(tbodyNode, configObj, data)
+        createTableHead()
+        createTableBody(data)
         enableSorting()
     }
 }
 
-function createTableHead(theadNode, configObj) {
+function createTableHead() {
     let theadDataString = '<tr>';
-    configObj.forEach((el) => {
+    TABLE_CONFIG.forEach((el) => {
         if (el.isSortable) {
-            sortingColumn = el.dataKey
+            SORT_COLUMN = el.dataKey
         }
         theadDataString += `<th ${el.isSortable ? 'class="sortable"' : ''}>
             <div>${el.columnName}${el.isSortable ? '<span class="sort-icon">&uarr;&darr;</span>' : ''}</div>
         </th>`
     })
     theadDataString += '</tr>';
-    theadNode.innerHTML = theadDataString
+    THEAD.innerHTML = theadDataString
 }
 
-function createTableBody(tbodyNode, configObj, data) {
-    let tBodyDataString = '';
+function createTableBody(data) {
+    let tBodyDataString = ''
     data.forEach((el) => {
-        tBodyDataString += '<tr>';
-        configObj.forEach((col) => {
+        tBodyDataString += '<tr>'
+        TABLE_CONFIG.forEach((col) => {
             tBodyDataString += `<td>${el[col.dataKey]}</td>`
         })
-        tBodyDataString += '</tr>';
+        tBodyDataString += '</tr>'
     })
-    tbodyNode.innerHTML = tBodyDataString
+    TBODY.innerHTML = tBodyDataString
 }
 
 function enableSorting() {
     document.querySelectorAll('.sortable').forEach((column) => {
         column.addEventListener('click', function handleClick() {
-            sortOrder = sortOrder > 0 ? -1 : 1;
-            column.querySelectorAll('.sort-icon')[0].innerHTML = sortOrder > 0 ? '&darr;' : '&uarr;';
-            tableData = sortData(tableData)
-            createTableBody(tbody, config, tableData)
+            SORT_ORDER = SORT_ORDER > 0 ? -1 : 1
+            column.querySelectorAll('.sort-icon')[0].innerHTML = SORT_ORDER > 0 ? '&darr;' : '&uarr;'
+            TABLE_DATA = sortData(TABLE_DATA)
+            createTableBody(TABLE_DATA)
         });
     });
 }
 
-function sortData(data) {
-    return data.sort(function (a, b) {
-        if (sortOrder > 0)
-            return a[sortingColumn].toLowerCase() > b[sortingColumn].toLowerCase() ? 1 : -1
-        else if (sortOrder < 0)
-            return a[sortingColumn].toLowerCase() < b[sortingColumn].toLowerCase() ? 1 : -1
+function sortData(_data) {
+    return _data.sort(function (_a, _b) {
+        if (SORT_ORDER > 0)
+            return _a[SORT_COLUMN].toLowerCase() > _b[SORT_COLUMN].toLowerCase() ? 1 : -1
+        else if (SORT_ORDER < 0)
+            return _a[SORT_COLUMN].toLowerCase() < _b[SORT_COLUMN].toLowerCase() ? 1 : -1
     })
 }
 
-async function init() {
-    tableData = await fetchApiData(apiUrl)
-    renderTable(thead, tbody, config, tableData)
+function cleanSlate() {
+    THEAD.innerHTML = ''
+    TBODY.innerHTML = ''
+    SORT_COLUMN = ''
+    API_URL = ''
+    TABLE_CONFIG = {}
+    SORT_ORDER = 1
 }
-
-init();
